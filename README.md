d5c23a57cae58cf463ab96ea89979e4e4e268383
KittyBorgX <krishnaramasimha12@gmail.com>
2021-07-29 13:04:48 +0530

    Initial Commit - Add Files

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # says-discord-bot
 An automated simon says bot for discord and other games :)
+
+### How to host locally: 
+- Clone this repo
+- Fill in the details in `config/example.config.json` and rename it to config.json
+- Run : `yarn start` to start the bot  
---
+++ index.js
@@ -0,0 +1,14 @@
+const { Client } = require("discord.js");
+const client = new Client();
+const { BOT_TOKEN } = require("./config/config.json");
+
+client.once("ready", () => {
+  console.log("Bot started");
+  client.user.setPresence({
+    type: "PLAYING",
+    activity: { name: "simon says" },
+    status: "dnd",
+  });
+});
+
+client.login(BOT_TOKEN);
